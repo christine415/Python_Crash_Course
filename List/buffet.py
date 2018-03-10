65a762015e71271617d1eb203349c2f31e700c63
christine415 <yichaochen415@gmail.com>
2018-03-10 18:14:13 -0500

    tuple

---
+++ List/buffet.py
@@ -0,0 +1,8 @@
+food = ("pizza", "sashimi", "fruit", "cake", "juice")
+print("The restaurant offers:")
+for f in food:
+    print(f)
+food = ("pizza", "sashimi", "fruit", "pasta", "ice cream")
+print("The resaurant now offers:")
+for f in food:
+    print(f)
