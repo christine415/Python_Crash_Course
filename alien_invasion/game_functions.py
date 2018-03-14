010dab0243bbc54675e3af3c91c33ed949bc20bd
christine415 <yichaochen415@gmail.com>
2018-03-13 20:38:03 -0400

    Pilot the ship

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -1,11 +1,32 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     '''Respond the keyboard and mouse events'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event,ship)
+
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+def check_keydown_events(event, ship):
+    '''Respond to keypress'''
+    if event.key == pygame.K_RIGHT:
+        # Move the ship to the right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event,ship):
+    '''Respond to key releases'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship, alien):
     # Redraw the screen during each pass through the loop
