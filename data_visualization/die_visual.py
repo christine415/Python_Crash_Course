cabb7fdb4c31ee7a1a0675e0e7ae597fdb437e43
christine415 <yichaochen415@gmail.com>
2018-03-13 22:07:51 -0400

    Use Pygal to visualize the distribution of rolling a die

---
+++ data_visualization/die_visual.py
@@ -0,0 +1,29 @@
+from die import Die
+import pygal
+
+# Create two dice
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls and save the results
+results = []
+for roll in range(50000):
+    result = die_1.roll()+die_2.roll()
+    results.append(result)
+
+# Analyze the results
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a D6 and a D10 50000 times." 
+hist.x_labels = [str(i) for i in range(2,max_result+1)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+hist.add('D6 + D10', frequencies) 
+hist.render_to_file('D6_D10_visual.svg')
