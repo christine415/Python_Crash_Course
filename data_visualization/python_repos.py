76da528a324729282c31f0c24e548082cc6288cb
christine415 <yichaochen415@gmail.com>
2018-03-14 21:57:28 -0400

    add more descriptions and an html link on the bar for each repository

--- data_visualization/python_repos.py
+++ data_visualization/python_repos.py
@@ -16,10 +16,16 @@ print("Total Repositories: " + str(response_dict['total_count']))
 
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append (repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url']
+    }
+    plot_dicts.append(plot_dict)
 
 # Make visualization
 my_style = LS('#333366', base_style=LCS)
@@ -38,5 +44,5 @@ chart = pygal.Bar(my_config, style = my_style)
 chart.title = 'Most-Starred Python Project on Github'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
